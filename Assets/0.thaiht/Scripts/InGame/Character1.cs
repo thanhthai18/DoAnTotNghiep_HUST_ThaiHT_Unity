538caa70d796f518ca79731ced701b73a25be4a2
thanhthai18 <hathanhthai18@gmail.com>
2023-06-07 20:58:59 +0700

    charater script animation

---
+++ Assets/0.thaiht/Scripts/InGame/Character1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace thaiht20183826
+{
+    public class Character1 : CharacterScript
+    {
+
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+        #region SUBSCRIBE
+        protected override void OnEnable()
+        {
+
+        }
+        protected override void OnDisable()
+        {
+
+        }
+        #endregion
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+
+    }
+}
---
+++ Assets/0.thaiht/Scripts/InGame/CharacterScript.cs
@@ -0,0 +1,66 @@
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace thaiht20183826
+{
+    public class CharacterScript : MonoBehaviour
+    {
+        [SerializeField] SkeletonAnimation anim;
+        [SpineAnimation] public string anim_idle, anim_run, anim_skill, anim_die;
+        [SpineSkin] public List<string> listSkin = new List<string>();
+
+        [SerializeField] bool isSpine;
+
+
+        protected virtual void Awake()
+        {
+            isSpine = false;
+            anim = GetComponent<SkeletonAnimation>();
+            if (anim != null)
+            {
+                isSpine = true;
+            }
+        }
+        #region SUBSCRIBE
+        protected virtual void OnEnable()
+        {
+
+        }
+        protected virtual void OnDisable()
+        {
+
+        }
+        #endregion
+
+        protected virtual void Start()
+        {
+            if (isSpine)
+            {
+                anim.AnimationState.Complete += AnimComplete;
+            }
+        }
+
+        protected virtual void AnimComplete(Spine.TrackEntry trackEntry)
+        {
+            //if (trackEntry.Animation.Name == anim_VaCham)
+            //{
+            //    PlayAnim(anim, anim_Idle, true);
+
+            //    //if (SoundController.instance != null)
+            //    //    SoundController.instance.PlayOneShot(SoundController.instance.soundFx_Minigame, SoundController.instance.nhacdinh);
+            //}
+        }
+
+        protected virtual void PlayAnim(SkeletonAnimation anim, string nameAnim, bool loop)
+        {
+            if (isSpine)
+            {
+                anim.state.SetAnimation(0, nameAnim, loop);
+            }
+        }
+
+        
+    }
+}
